3474dad280049e4417a535b9422465438c9c5671
rogeriohenning <128552442+rogeriohenning@users.noreply.github.com>
2023-10-18 07:59:01 -0300

    sombrasil

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function tocaSomMario(){
     document.querySelector('#som_tecla_mario').play();
 }
 document.querySelector('.tecla_mario') .onclick=tocaSomMario;
+
+function tocaSomBrasil(){
+    document.querySelector('#som_tecla_brasil').play();
+    }
+document.querySelector('tecla_brasil') .onclick=tocaSomBrasil;
